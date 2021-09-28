d8a3dce639a0c4ce22ae752170800ad1e962087b
jayasanloki <jayasanloki@gmail.com>
2021-09-28 23:05:47 +0530

    selenium testing

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 import time
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 print("sample test case started")
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver=webdriver.firefox()
 #driver=webdriver.ie()
 #maximize the window size
 driver.maximize_window()
 #navigate to the url
-driver.get("https://www.google.com/")
+driver.get("https://www.youtube.com/")
 #identify the Google search text box and enter the value
-driver.find_element_by_name("q").send_keys("javatpoint")
+driver.find_element_by_name("search_query").send_keys("petta")
 time.sleep(3)
 #click on the Google search button
 driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
