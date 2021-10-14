a92f40f343d983b1d0106d3d561e6e28c980b386
jayasanloki <jayasanloki@gmail.com>
2021-10-14 16:38:07 +0530

    optimized code, exception handling pending.

--- CS360_access_sr.py
+++ CS360_access_sr.py
@@ -85,7 +85,7 @@ EIM Team'''
 
 body=part1+part2+part3
 msg.attach(MIMEText(body, 'plain'))
-s = smtplib.SMTP('appsmtp-north.internal.vodafone.com:25')
+s = smtplib.SMTP('')
 s.ehlo
 text =  msg.as_string()
 #s.sendmail(fromaddr, (toaddr+tocc).split(';'), text)
